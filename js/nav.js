cf866362e81e42277feea2c55d53a1e4855a6e46
Jasius <jasius@Outlook.com>
2016-08-28 12:09:32 +0300

    A0828 RC-2

--- js/nav.js
+++ js/nav.js
@@ -107,3 +107,22 @@ function setText() {
   $('.changeBg').attr('value', $text);
 
 }
+/* hiding */
+$(function(){
+    var lastScrollTop = 0, delta = 5;
+    $(window).scroll(function(event){
+       var st = $(this).scrollTop();
+
+       if(Math.abs(lastScrollTop - st) <= delta)
+          return;
+
+       if (st > lastScrollTop){
+         // Scroll down
+         $('nav').addClass('nav-hide');
+       } else {
+         // Scroll up
+         $('nav').removeClass('nav-hide');
+       }
+       lastScrollTop = st;
+    });
+});
