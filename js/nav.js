ad8acfdab83fb09e15fb8a375d3d174c75603683
Mindaugas Jasiulis <jasius@outlook.com>
2017-03-23 00:02:06 +0200

    mobile improvements, taskrunner

--- js/nav.js
+++ js/nav.js
@@ -1,52 +1,54 @@
-/* responsive nav */
+// responsive nav
 var w = $(window).width(),
-    toggle 		= $('#toggle-menu'),
-    menu 		= $('nav ul'),
-    hasChild = $('.has-child'),
-    dropdown = $('.dropdown');
+  toggle = $('#toggle-menu'),
+  menu = $('nav ul'),
+  hasChild = $('.has-child'),
+  dropdown = $('.dropdown');
 
-$(function() {
-  $(toggle).on('click', function(e) {
+$(function () {
+  $(toggle).on('click', function (e) {
     e.preventDefault();
     menu.toggle();
   });
 
-  $(hasChild).click(function(e) {
+  $(hasChild).click(function (e) {
     e.preventDefault();
     dropdown.toggle();
   });
 });
 
-$(window).resize(function(){
-  if(w > 320 && menu.is(':hidden')) {
-    menu.removeAttr('style');}
+$(window).resize(function () {
+  if (w > 320 && menu.is(':hidden')) {
+    menu.removeAttr('style');
+  }
 });
 
-/* hover event */
+// hover events
 // cache elements
 var $active,
-    $indicator,
-    resetTimer;
+  $indicator,
+  resetTimer;
 // events
-$("nav .targeted").mouseover(function(){
+$("nav .targeted").mouseover(function () {
   clearTimeout(resetTimer);
   moveIndicator($(this));
-}).mouseout(function(){
-  resetTimer = setTimeout(function(){
+}).mouseout(function () {
+  resetTimer = setTimeout(function () {
     moveIndicator($active);
   }, 500);
 });
 // indicator move
-$(window).on('resize', function() {
+$(window).on('resize', function () {
   moveIndicator($active);
-  });
+});
 // init
-$(function(){
-	$active = $("#active");
+$(function () {
+  $active = $("#active");
   $indicator = $("#indicator");
   moveIndicator($active);
 });
-function moveIndicator($elm){
+
+function moveIndicator($elm) {
   $indicator.css({
     left: $elm.position().left,
     width: $elm.width(),
@@ -54,22 +56,23 @@ function moveIndicator($elm){
   })
 }
 
-/* hiding */
-$(function(){
-    var lastScrollTop = 0, delta = 5;
-    $(window).scroll(function(event){
-       var st = $(this).scrollTop();
+// hiding
+$(function () {
+  var lastScrollTop = 0,
+    delta = 5;
+  $(window).scroll(function (event) {
+    var st = $(this).scrollTop();
 
-       if(Math.abs(lastScrollTop - st) <= delta)
-          return;
+    if (Math.abs(lastScrollTop - st) <= delta)
+      return;
 
-       if (st > lastScrollTop){
-         // Scroll down
-         $('nav').addClass('nav-hide');
-       } else {
-         // Scroll up
-         $('nav').removeClass('nav-hide');
-       }
-       lastScrollTop = st;
-    });
+    if (st > lastScrollTop) {
+      // Scroll down
+      $('nav').addClass('nav-hide');
+    } else {
+      // Scroll up
+      $('nav').removeClass('nav-hide');
+    }
+    lastScrollTop = st;
+  });
 });
