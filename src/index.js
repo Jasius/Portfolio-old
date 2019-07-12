0b5bcf674c47b2f213fcea8d6bdccc2bbdb1594b
Mindaugas Jasiulis <jasius@outlook.com>
2019-07-12 20:34:52 +0300

    projects section fetching error handling

--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ getTimespan('1/4/2016', 'experienceAge')
 document.getElementById('toggle-menu').addEventListener('click', event => { toggleElementVisibility('panel') })
 document.getElementById('copyrightDate').innerText = new Date().getFullYear()
 
-fetch('https://gist.githubusercontent.com/Jasius/20d325502a0c55b1f6000fc855b09274/raw/data.json').then(response => response.json()).then(data => {
-  Render.byId(getProjectsCards(data.projects), 'projectsView')
+fetch('https://gist.githubusercontent.com/Jasius/20d325502a0c55b1f6000fc855b09274/raw/data.json')
+.then(response => response.json())
+.then(data => {
+  Render.byId(getProjectsCards(data.projects), 'projectsView') 
+})
+.catch(function (error) {
+  Render.byId('<h1>Failed to load projects information</h1>', 'projectsView')
+  console.error(error)
 })
